a5ced48b9f3a1db8285b133ee30ad43f0fc6431d
CMMNCT <mark@cmmnct.nl>
2023-11-30 16:44:02 +0100

    shuffle

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
 const data = {
-     images : [
-    { name: "cow" },
-    { name: "horse" },
-    { name: "goat" },
-    { name: "hen" },
-    { name: "dog" },
-    { name: "duck" },
-    { name: "koe" },
-    { name: "pig" },
-    { name: "cat" },
+    images: [
+        { name: "cow" },
+        { name: "horse" },
+        { name: "goat" },
+        { name: "hen" },
+        { name: "dog" },
+        { name: "duck" },
+        { name: "pig" },
+        { name: "cat" },
     ],
-    players : [
-    {
-        name: "Jack",
-        score :  0
-    },
-    {
-        name: "Jill",
-        score: 0
+    players: [
+        {
+            name: "Jack",
+            score: 0
+        },
+        {
+            name: "Jill",
+            score: 0
         }
     ]
 }
@@ -26,7 +25,9 @@ const gameField = document.getElementById("game-field");
 let currentPlayer = Math.random() < 0.5 ? data.players[0] : data.players[1];
 
 function init() {
-    data.images.forEach(image => {
+
+    let cardset = shuffle(data.images.concat(data.images))
+    cardset.forEach(image => {
         let newCard = document.createElement("div");
         let newImage = document.createElement("img");
         let cover = document.createElement("div");
@@ -37,16 +38,38 @@ function init() {
         newCard.appendChild(cover);
         gameField.appendChild(newCard);
 
-        
+
     })
 
 }
-function onTurnCard() {
+function onTurnCard(event) {
+    if (event.target.className === "cover") {
+        console.log("card clicked");
+        event.target.className = "";
+    }
 
 }
 
 function checkMatch() {
 
+}
+
+function shuffle(array) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+    }
+
+    return array;
 }
 
 init();
