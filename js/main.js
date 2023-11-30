d5f3249ca80817f96f51dbcaecc5f78b3b45c039
CMMNCT <mark@cmmnct.nl>
2023-11-30 18:00:26 +0100

    timers

--- js/main.js
+++ js/main.js
@@ -20,11 +20,16 @@ const data = {
         }
     ],
     card1: undefined,
-    card2: undefined
+    card2: undefined,
+    time: {
+        min: 0,
+        sec: 0
+    }
 }
 
 const gameField = document.getElementById("game-field");
 let currentPlayer = Math.random() < 0.5 ? data.players[0] : data.players[1];
+let timer = undefined;
 
 function init() {
 
@@ -42,6 +47,10 @@ function init() {
 
 
     })
+    timer = setInterval(() => {
+        console.log("tick");
+        updateClock(data.time);
+    }, 1000);
 
 }
 function onTurnCard(event) {
@@ -53,26 +62,23 @@ function onTurnCard(event) {
         console.log("card 2 clicked");
         event.target.className = "";
         data.card2 = event.target.parentNode;
-        if (data.card1.dataset.card === data.card2.dataset.card) {
-            currentPlayer.score++;
-            data.card1.style.visibility = "hidden";
-            data.card2.style.visibility = "hidden";
-        } else {
-            console.log("else called")
-            data.card1.children[1].className = "cover";
-            data.card2.children[1].className = "cover";
-        }
-        data.card1 = undefined;
-        data.card2 = undefined;
-        currentPlayer = currentPlayer === data.players[0]? data.players[1] : data.players[0]
+        setTimeout(() => checkMatch(), 2000);
     }
-
-
-
-
 }
 
 function checkMatch() {
+    if (data.card1.dataset.card === data.card2.dataset.card) {
+        currentPlayer.score++;
+        data.card1.style.visibility = "hidden";
+        data.card2.style.visibility = "hidden";
+    } else {
+        console.log("else called")
+        data.card1.children[1].className = "cover";
+        data.card2.children[1].className = "cover";
+    }
+    data.card1 = undefined;
+    data.card2 = undefined;
+    currentPlayer = currentPlayer === data.players[0] ? data.players[1] : data.players[0]
 
 }
 
@@ -92,6 +98,17 @@ function shuffle(array) {
     }
 
     return array;
+}
+
+function updateClock(time) {
+    time.sec++
+    if (time.sec === 60) {
+        time.sec = 0;
+        time.min++
+    }
+    seconds = time.sec < 10 ? seconds = `0${time.sec}` : time.sec
+    console.log(`${time.min} : ${seconds}`)
+
 }
 
 init();
